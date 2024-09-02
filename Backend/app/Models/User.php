054cb6f4e3a602f19645f7bd1d23922e2bf406b5
MateuszCzz <mateuszczernic.mc@gmail.com>
2024-09-02 16:03:34 +0200

    Overwrite sanctum PersonalAccessToken

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -8,10 +8,75 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
+use Laravel\Sanctum\PersonalAccessToken;
+use App\Enum\TokenAbility;
+use App\Enum\TokenName;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
+
+    /**
+     * Simplified Override from HasApiTokens trait.
+     * Create a new access token based on enum values and config settings.
+     * @param bool|null  $isRememberMeToken Whether to create a "remember me" token.
+     * @return \Laravel\Sanctum\NewAccessToken
+     */
+    public function createToken(bool $isRememberMeToken = false): NewAccessToken
+    {
+        $tokenName = $isRememberMeToken ? TokenName::REMEMBER_ME_ACCESS_TOKEN->value : TokenName::ACCESS_TOKEN->value;
+        $expiresAt = config('sanctum.ac_expiration') == null ? null : now()->addMinutes(config('sanctum.ac_expiration'));
+
+        $plainTextToken = $this->generateTokenString();
+        $token = $this->tokens()->create([
+            'name' => $tokenName,
+            'token' => hash('sha256', $plainTextToken),
+            'abilities' => [TokenAbility::ACCESS_API->value],
+            'expires_at' => $expiresAt,
+        ]);
+
+        return new NewAccessToken($token, $token->getKey() . '|' . $plainTextToken);
+    }
+
+    /**
+     * Create a new refresh token.
+     * @param  \Laravel\Sanctum\PersonalAccessToken  $referenceToken Token the newly created refresh token will reference.
+     * @return \Laravel\Sanctum\NewAccessToken|null
+     */
+    public function createRefreshToken(PersonalAccessToken $referenceToken): NewAccessToken|null
+    {
+        // Set token params based on the type of referenced token
+        switch ($referenceToken->name) {
+            case TokenName::ACCESS_TOKEN->value:
+                $expiresAt = config('sanctum.rf_expiration');
+                $tokenName = TokenName::REFRESH_TOKEN->value;
+                break;
+
+            case TokenName::REMEMBER_ME_ACCESS_TOKEN->value:
+                $expiresAt = config('sanctum.remember_me_rf_expiration');
+                $tokenName = TokenName::REMEMBER_ME_REFRESH_TOKEN->value;
+                break;
+
+            default:
+                // Wrong token was passed in reference 
+                return null;
+        }
+
+        // If config value is null, set expiration to null
+        $expiresAt = $expiresAt !== null ? now()->addMinutes($expiresAt) : null;
+
+        $plainTextToken = $this->generateTokenString();
+        $token = $this->tokens()->create([
+            'name' => $tokenName,
+            'token' => hash('sha256', $plainTextToken),
+            'abilities' => [TokenAbility::REFRESH_EXPIRATION->value],
+            'expires_at' => $expiresAt,
+            'reference_token_id' => $referenceToken->id,
+        ]);
+
+        return new NewAccessToken($token, $token->getKey() . '|' . $plainTextToken);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -43,6 +108,9 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
---
+++ Backend/database/migrations/2024_09_01_092539_add_reference_token_id_to_personal_access_tokens_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->foreignId('reference_token_id')->after('abilities')->nullable()->constrained('personal_access_tokens')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('reference_token_id');
+        });
+    }
+};
